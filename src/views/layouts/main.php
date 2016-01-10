13bd837c20e98ed0ecbef6dc48894d22f55e2e33
Abhinav <in.abhi9@gmail.com>
2016-01-10 11:38:44 +0530

    Clean up

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -30,7 +30,7 @@ AppAsset::register($this);
     <?php
     NavBar::begin(
         [
-            'brandLabel' => 'My Company',
+            'brandLabel' => 'Property Listing',
             'brandUrl'   => Yii::$app->homeUrl,
             'options'    => [
                 'class' => 'navbar-inverse navbar-fixed-top',
@@ -42,17 +42,11 @@ AppAsset::register($this);
             'options' => ['class' => 'navbar-nav navbar-right'],
             'items'   => [
                 ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
-                Yii::$app->user->isGuest
-                    ?
-                    ['label' => 'Login', 'url' => ['/site/login']]
-                    :
-                    [
-                        'label'       => 'Logout (' . Yii::$app->user->identity->username . ')',
-                        'url'         => ['/site/logout'],
-                        'linkOptions' => ['data-method' => 'post']
-                    ],
+                [
+                    'label'       => 'HackerEarth Event',
+                    'url'         => 'https://www.hackerearth.com/propertyguru-full-stack-developer-hiring-challenge/',
+                    'linkOptions' => ['target' => '_blank']
+                ],
             ],
         ]
     );
@@ -71,8 +65,6 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
